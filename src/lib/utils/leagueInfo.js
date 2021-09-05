95d91dfb52841f1c9e7a776ee9622a761158566f
rockrit-app <andrewrickert28@gmail.com>
2021-09-05 16:19:57 +1000

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -16,6 +16,288 @@ export const homepageText = `
 <p>You know the rules. You know the spirit of the competition. You know your obligations and responsibilities – the most important of which are to yourselves. I believe that after 16 weeks of competition we will all be able to look back on what we have achieved, together, and reflect on the fact that the real fantasy football championship was the friendships and rivalries we made along the way. </p>
 <p>Go well. Love you.</p>
 <p>Commissioner</p>
+
+<h6>Guest Post - Expert Analyst Hugo McWilliam</h6>
+<p class="MsoNormal"><b><u><span lang="EN-US" style="font-size:14.0pt;line-height:
+107%;mso-ansi-language:EN-US">East Division<o:p></o:p></span></u></b></p>
+
+<p class="MsoNormal"><b><span lang="EN-US" style="mso-ansi-language:EN-US">Toby
+Honig<o:p></o:p></span></b></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Helped by
+some amazing keeper selections, Toby’s draft performance was solid … but
+uninspiring. Tyreek Hill and <span class="SpellE">Saquon</span> Barkley with the
+first two picks will undoubtedly show some beating this season, but there are
+some clear weaknesses in his squad. Draft Score: B<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Best Pick: Can’t
+decide between Calvin Ridley, DK <span class="GramE">Metcalf</span> and Josh
+Allen – all outstanding keeper picks kept well below ADP. His Wide receiver
+corps will take some beating.<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Worst Pick:
+Melvin Gordon in the fifth round and James Conner in the eighth round. YUCK! If
+<span class="SpellE">Saquon’s</span> health is an issue this year, Toby’s running
+back room will struggle.<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Best Keeper:
+<span class="SpellE">Jakobi</span> Meyers and Mac Jones taken very late in the
+draft. A potential QB/WR stack which could reap rewards in future years<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Predicted
+Division Standing: 2<sup>nd</sup> – Hard to drop the defending champ down any
+further but I don’t think his team is as good as last year.<o:p></o:p></span></p>
+
+<p class="MsoNormal"><b><span lang="EN-US" style="mso-ansi-language:EN-US">Hugo
+McWilliam<o:p></o:p></span></b></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Can’t see
+this squad doing anything special. Draft Score: B-<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Best Pick: Austin
+<span class="SpellE">Ekeler</span> in the first round.<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Worst Pick:
+Kenny <span class="SpellE">Golladay</span> in the sixth round is very risky<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Best Keeper:
+Trevor Lawrence will be one to watch in 2022. Maybe not this season though.<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Predicted
+Division Standing: 4<sup>th</sup> – An average team in the scheme of things<o:p></o:p></span></p>
+
+<p class="MsoNormal"><b><span lang="EN-US" style="mso-ansi-language:EN-US">Anthony <span class="SpellE">Penklis</span><o:p></o:p></span></b></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Has done
+his research over the off season and it showed. A very solid draft from <span class="SpellE">Penklis</span>. Draft Score: B-<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Best Pick: Travis
+Kelce in the first round. It takes guts to pick a tight end that early, but <span class="SpellE">Penklis</span> now has the best player in that position for the
+whole season<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Worst Pick:
+JK Dobbins in the seventh round. Beyond his control, and so unfair by others,
+but unfortunately this will go down as a lowlight<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Best Keeper:
+<span class="GramE">Unfortunately</span> he has very few options, hard to see who
+he is going to keep and he may struggle in that department going forward<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Predicted
+Division Standing: 5<sup>th</sup> – tough division but will push for playoffs<o:p></o:p></span></p>
+
+<p class="MsoNormal"><b><span lang="EN-US" style="mso-ansi-language:EN-US">Andrew
+Rickert<o:p></o:p></span></b></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">His draft
+had a rhythm to it … One pick good, one pick bad … went like that the whole way
+through so we’ll give him an average score. Draft Score: C<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Best Pick: Keeping
+TJ <span class="SpellE">Hockenson</span> in the 11<sup>th</sup> round is an
+absolute steal … has potential to be a top five TE this season<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Worst Pick:
+Rickert picked Robert Woods and Cooper <span class="SpellE">Kupp</span> <span class="GramE">back to back</span> in the third and fourth rounds. These weren’t
+bad picks at all, in fact they could’ve been amazing if he had chosen to take
+Matthew Stafford in the sixth round … instead he took <span class="SpellE">JuJu</span>
+Smith-Schuster and I almost vomited. Looking forward to Rickert doing the vomit
+challenge with <span class="SpellE">JuJu</span> this season on Tik Tok.<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Best Keeper:
+Marquez Callaway in the 12<sup>th</sup> round was a great selection, especially
+if Jameis stays on the field<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Predicted
+Division Standing: 6<sup>th</sup> – tough division for Rickert and I don’t see
+him challenging.<o:p></o:p></span></p>
+
+<p class="MsoNormal"><b><span lang="EN-US" style="mso-ansi-language:EN-US">Alex
+Glendenning<o:p></o:p></span></b></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Impressed
+throughout, very solid performance – well done! Draft Score: A+<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Best Pick: Dak
+Prescott in the fifth round and Michael Gallup in the tenth round were both
+amazing values. A great QB/WR Stack<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Worst Pick:
+Devonta Smith was very early, but worst pick will probably be Rashaad Penny.
+Shows how good his draft was that worst pick was in the 12<sup>th</sup> round.<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Best Keeper:
+Justin Fields in the 11<sup>th</sup> round is an amazing steal. Has a
+guaranteed QB1 in 2022, plus this season, if Fields starts, he’s stacked with
+Allen Robinson<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Predicted
+Division Standing: 1<sup>st</sup> – <span class="GramE">Yes</span> I’m surprised
+too.<o:p></o:p></span></p>
+
+<p class="MsoNormal"><b><span lang="EN-US" style="mso-ansi-language:EN-US">Jack
+Holloway<o:p></o:p></span></b></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Pretty good
+… would’ve been an A except he made some crucial errors. Draft Score: B-<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Best Pick: Both
+keeper selections but Terry McLaurin and Tom Brady represent amazing value<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Worst Pick:
+In my opinion the second worst pick of the entire draft (behind <span class="SpellE">Straton</span>) – Gus Edwards in the third round was atrocious.
+Holloway’s justification was there were no running backs left …have a look at <span class="SpellE">Kelland’s</span>, <span class="SpellE">Penklis</span>’ or Taylor’s
+selections and tell me that’s true!!<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Best Keeper:
+Trey Lance in the 11<sup>th</sup> round will be a steal in 2022. Word of
+warning though, remember Holloway dropped Cam Akers in 2020.<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Predicted
+Division Standing: 3<sup>rd</sup> – If Christian McCaffrey stays <span class="GramE">fit</span> he’ll be hard to beat<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US"><o:p>&nbsp;</o:p></span></p>
+
+<p class="MsoNormal"><b><u><span lang="EN-US" style="font-size:14.0pt;line-height:
+107%;mso-ansi-language:EN-US">North Division<o:p></o:p></span></u></b></p>
+
+<p class="MsoNormal"><b><span lang="EN-US" style="mso-ansi-language:EN-US">Alex
+Taylor<o:p></o:p></span></b></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Couldn’t <span class="SpellE">capitalise</span> on his strong keepers. Everyone selected was
+pretty meh. Draft score: B-<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Best Pick: Hard
+to go past Antonio Gibson, Justin <span class="GramE">Jefferson</span> and Joe
+Burrow. The best keepers in the competition<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Worst Pick:
+DJ Moore in the fourth round. He might be a metaphor for Taylor’s 2021 season,
+so much potential, but will fall short of expectation<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Best Keeper:
+Alongside his already strong keepers, Taylor has added Zach Wilson. Won’t be
+much value this year but in 2022 will be one to watch.<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Predicted
+Division Standing: 2<sup>nd</sup> – Lucky he’s in a weak division<o:p></o:p></span></p>
+
+<p class="MsoNormal"><b><span lang="EN-US" style="mso-ansi-language:EN-US">Daniel <span class="SpellE">Straton</span><o:p></o:p></span></b></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">From picking
+Nick Chubb in the first round, to an unbelievable gaffe in the second round, <span class="SpellE">Straton’s</span> draft performance was utterly horrendous!! Draft
+score: F<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Best Pick: Getting
+Keenan Allen in the fourth round via a keeper selection was a rare highlight
+during a terrible performance<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Worst Pick:
+Oh god, where do we start? Nick Chubb with the seventh overall pick was a reach,
+but undoubtedly, the worst pick was his <span class="GramE">second round</span>
+selection of Patrick <span class="SpellE">Mahomes</span>. Let’s be honest,
+calling it a selection is generous. <span class="SpellE">Straton</span> wound
+down the clock and waited for the computer to pick Tyler Boyd. He then sat and waited
+some more for Patrick <span class="SpellE">Kelland</span> to pick Patrick <span class="SpellE">Mahomes</span> and chucked a tantrum two picks after his allocated
+turn. The commissioner, not wanting another <span class="SpellE">Straton</span>
+tantrum, opted to let <span class="SpellE">Straton</span> pick … guess who? …
+Patrick <span class="SpellE">Mahomes</span>! Irony is, <span class="SpellE">Straton</span>
+<span class="GramE">actually took</span> <span class="SpellE">Mahomes</span> well
+above ADP – should’ve taken David Montgomery or Chris Carson at the same spot.<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Best Keeper:
+Slim Pickings, but Baker Mayfield can be kept for a 12<sup>th</sup> rounder
+next season. Watch this space<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Predicted
+Division Standing: 6<sup>th</sup> – will be battling it out with Lysaght for
+the toilet bowl this year<o:p></o:p></span></p>
+
+<p class="MsoNormal"><b><span lang="EN-US" style="mso-ansi-language:EN-US">Pat <span class="SpellE">Kelland</span><o:p></o:p></span></b></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Impressive
+the whole way through. Didn’t miss at all. Draft Score: A-<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Best Pick: Javonte
+Williams in the fourth round is solid but the revenge factor makes it a belter
+of a pick: Pat hates Melvin Gordon! Williams will overtake Gordon this season!<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Worst Pick:
+Taking Patrick <span class="SpellE">Mahomes</span> in the second round … lucky
+for Pat this pick was overruled by <span class="SpellE">Straton</span><o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Best Keeper:
+Elijah Moore in the 12<sup>th</sup> round. By all reports – this guy is a gun.
+Maybe not this year, but <span class="GramE">definitely next</span>.<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Predicted
+Division Standing: 1<sup>st</sup> – Rags to riches – good luck Pat.<o:p></o:p></span></p>
+
+<p class="MsoNormal"><b><span lang="EN-US" style="mso-ansi-language:EN-US">Alex
+Corsaro<o:p></o:p></span></b></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Very
+uninspiring. Draft Score: C<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Best Pick: Stefon
+Diggs in the fourth round and Aaron Rodgers in the fifth round were possibly
+Corsaro’s only good picks of the draft.<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Worst Pick:
+A few to choose from. Kyle Pitts in the third round takes the cake for me
+though. Takes away any keeper value and god he has a lot of hype considering he
+was taken before Mark Andrews and Darren Waller.<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Best Keeper:
+Robby Anderson in the 14<sup>th</sup> round is probably his only viable future
+keeper pick<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Predicted
+Division Standing: 4<sup>th</sup> – disappointing draft … and we haven’t even
+included him sticking up for <span class="SpellE">Straton</span>.<o:p></o:p></span></p>
+
+<p class="MsoNormal"><b><span lang="EN-US" style="mso-ansi-language:EN-US">Andrew <span class="SpellE">Selbie</span><o:p></o:p></span></b></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Not bad <span class="SpellE">Selbie</span> … not good though. Draft score: C<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Best Pick: Michael
+Pittman in the eighth round or Cole Beasley in the 14<sup>th</sup> round. <span class="SpellE">Selbie</span> has a very deep wide receiver roster<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Worst Pick:
+Michael Thomas in the fifth round. Thomas may come good in time for the
+playoffs, only problem is, <span class="SpellE">Selbie</span> likely won’t be
+there<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Best Keeper:
+I genuinely have no idea who <span class="SpellE">Selbie</span> will keep. Can’t
+see any good option yet.<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Predicted
+Division Standing: 3<sup>rd</sup> – Once again, third in the North division
+probably means he will finish 8<sup>th</sup> overall.<o:p></o:p></span></p>
+
+<p class="MsoNormal"><b><span lang="EN-US" style="mso-ansi-language:EN-US">Thomas
+Lysaght<o:p></o:p></span></b></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Lysaght
+loves the D … also I felt bad giving him an F <span class="GramE">cause</span> I
+haven’t seen him in a bit. Draft score: D<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Best Pick: Dalvin
+Cook in the first round. Only time Lysaght picked the right player in the right
+spot.<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Worst Pick:
+Keeping Kareem Hunt and Ronald Jones in the fourth and fifth rounds
+respectively. Oh god, could be a long season<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Best Keeper:
+Ryan Green in the 15<sup>th</sup> round. Perhaps the steal of the draft, will
+rip it up for … <span class="SpellE">uhh</span> … who does he play for again?<o:p></o:p></span></p>
+
+<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language:EN-US">Predicted
+Division Standing: 5<sup>th</sup> – Will hopefully beat <span class="SpellE">Straton</span><o:p></o:p></span></p>
+
 `;
 
 /*   STEP 3   */
