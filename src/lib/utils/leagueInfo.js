2d02a78ac8af0f5ba9c15735ece3d7021025bf32
rockrit-app <andrewrickert28@gmail.com>
2021-09-05 14:50:14 +1000

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,8 +1,8 @@
 /*   STEP 1   */
-export const leagueID = "your_league_id"; // 737108337161793536
-export const leagueName = "your_league_name"; // 2020-21 League
+export const leagueID = "your_league_id"; 737108337161793536
+export const leagueName = "your_league_name"; "2020-21 League"
 export const dues = 100; // (optional) used in template constitution page
-export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
+export const dynasty = false;
 
 /*   STEP 2   */
 export const homepageText = `
